eb4143daa91d8754a6d7d5b92e5aef6212a6431a
yuya nishizawa <22aw0128@jec.ac.jp>
2022-12-14 22:00:08 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Routes,
   Route,
  } from "react-router-dom";
+import { useEffect, useState } from 'react';
  
 function Home(){
   return(
@@ -16,14 +17,16 @@ function Home(){
 }
 
 function Weather(){
-  const handleChange = ()=>{
-    console.log("テキスト")
+  const [val,setval] = useState();
+  const handleChange =(e)=>{
+    setval(e.target.value);
+    alert(`${e.target.value}が選択されました`);
   }
 
   return( 
     <>
       <h1>Weather</h1>
-      <select name='place' id='place' onChange={handleChange}>
+      <select name='place' id='place' value={val} onChange={handleChange}>
         <option value="tokyo">東京</option>
         <option value="osaka">大阪</option>
         <option value="sapporo">札幌</option>
